8c3652c0d2fc3cc6afaeb6e6e28336bb2112c942
Mr.Ender <72358826+MrEnder0@users.noreply.github.com>
2022-08-06 22:51:31 -0500

    Update main.nim

--- main.nim
+++ main.nim
@@ -5,26 +5,26 @@ const doc = """
 CoolSeq - a program to do things
 
 Usage:
-    args_docopt version
-    args_docopt v
-    args_docopt interpret <path>
-    args_docopt i <path>
+    args_docopt -version
+    args_docopt -v
+    args_docopt -interpret <path>
+    args_docopt -i <path>
 
 Options:
-    version                     Returns language version
-    v                           Returns language version
-    interpret                   Interpretes the code at the given path
-    i                           Interpretes the code at the given path
+    -version                     Returns language version
+    -v                           Returns language version
+    -interpret                   Interpretes the code at the given path
+    -i                           Interpretes the code at the given path
     <path>                      Specify path to input files
 """
 
 proc main() =
     let args = docopt(doc, version = "1.0")
 
-    if args["version"] or args["v"]:
+    if args["-version"] or args["-v"]:
         echo "0.0.2"
 
-    if args["interpret"] or args["i"]:
+    if args["-interpret"] or args["-i"]:
         echo code_parser.parse($args["<path>"])
 
 when isMainModule:
