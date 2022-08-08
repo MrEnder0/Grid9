196b76ce84a79c4561bfbe59cc6758e7868857e4
Mr.Ender <72358826+MrEnder0@users.noreply.github.com>
2022-08-08 17:34:00 -0500

    memory grid

--- interpreter.nim
+++ interpreter.nim
@@ -1,2 +1,17 @@
+import std/strformat
+import tables
+
+iterator `...`*[T](a: T, b: T): T =
+    var res: T = T(a)
+    while res <= b:
+        yield res
+        inc res
+
 proc interpret*(parsed_code: string) : string {.discardable.} =
-    return "temp"
+    var mem_grid: Table[string, string]
+
+    for i in 0...9:
+        mem_grid[fmt"{i}"] = "0"
+
+    #test data in mem_grid
+    echo mem_grid["6"]
