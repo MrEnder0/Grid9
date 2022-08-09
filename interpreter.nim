ccdf07c1ee11844692cd2dec36dca73efa721435
Mr.Ender <72358826+MrEnder0@users.noreply.github.com>
2022-08-08 21:41:00 -0500

    Set command + other additions
    Set command
    Frame and final mem printing for debugging
    Mem grid size set to 9 like its supposed to

--- interpreter.nim
+++ interpreter.nim
@@ -13,17 +13,23 @@ proc interpret*(parsed_code: string) : string {.discardable.} =
     var c_index: int = 0
 
     #generate memory grid
-    for i in 0...9:
+    for i in 0...8:
         mem_grid[fmt"{i}"] = "0"
 
     while c_index + 1 < len(parsed_code):
+        #flip command
         if parsed_code[c_index] == 'f':
             if len(parsed_code) > c_index + 1:
                 if parseint(mem_grid[fmt"{parsed_code[c_index + 1]}"]) == 0:
                     mem_grid[fmt"{parsed_code[c_index + 1]}"] = "1"
                 else:
                     mem_grid[fmt"{parsed_code[c_index + 1]}"] = "0"
+        #set command
+        elif parsed_code[c_index] == 's':
+            if len(parsed_code) > c_index + 2:
+                mem_grid[fmt"{parsed_code[c_index + 1]}"] = fmt"{parsed_code[c_index + 2]}"
 
         c_index += 1
         #print mem_grid for debugging
-        echo mem_grid["0"] & mem_grid["1"] & mem_grid["2"] & mem_grid["3"] & mem_grid["4"] & mem_grid["5"] & mem_grid["6"] & mem_grid["7"] & mem_grid["8"] & mem_grid["9"]
+        echo "Frame mem: " & mem_grid["0"] & mem_grid["1"] & mem_grid["2"] & mem_grid["3"] & mem_grid["4"] & mem_grid["5"] & mem_grid["6"] & mem_grid["7"] & mem_grid["8"]
+    echo "Final mem: " & mem_grid["0"] & mem_grid["1"] & mem_grid["2"] & mem_grid["3"] & mem_grid["4"] & mem_grid["5"] & mem_grid["6"] & mem_grid["7"] & mem_grid["8"]
