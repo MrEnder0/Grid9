acc24ae3aac189e9ef61d5f0e914e6546e33f387
Mr.Ender <72358826+MrEnder0@users.noreply.github.com>
2022-08-08 20:56:39 -0500

    Update interpreter.nim

--- interpreter.nim
+++ interpreter.nim
@@ -10,8 +10,9 @@ iterator `...`*[T](a: T, b: T): T =
 proc interpret*(parsed_code: string) : string {.discardable.} =
     var mem_grid: Table[string, string]
 
+    #generate memory grid
     for i in 0...9:
         mem_grid[fmt"{i}"] = "0"
 
-    #test data in mem_grid
-    echo mem_grid["6"]
+    for index in 0...len(parsed_code):
+        echo parsed_code[index]
