a5721d1fb7a060e19189006a855fd6f9bb4a9f68
Mr.Ender <72358826+MrEnder0@users.noreply.github.com>
2022-08-08 21:31:09 -0500

    flip command

--- interpreter.nim
+++ interpreter.nim
@@ -1,4 +1,5 @@
 import std/strformat
+import strutils
 import tables
 
 iterator `...`*[T](a: T, b: T): T =
@@ -9,10 +10,20 @@ iterator `...`*[T](a: T, b: T): T =
 
 proc interpret*(parsed_code: string) : string {.discardable.} =
     var mem_grid: Table[string, string]
+    var c_index: int = 0
 
     #generate memory grid
     for i in 0...9:
         mem_grid[fmt"{i}"] = "0"
 
-    for index in 0...len(parsed_code):
-        echo parsed_code[index]
+    while c_index + 1 < len(parsed_code):
+        if parsed_code[c_index] == 'f':
+            if len(parsed_code) > c_index + 1:
+                if parseint(mem_grid[fmt"{parsed_code[c_index + 1]}"]) == 0:
+                    mem_grid[fmt"{parsed_code[c_index + 1]}"] = "1"
+                else:
+                    mem_grid[fmt"{parsed_code[c_index + 1]}"] = "0"
+
+        c_index += 1
+        #print mem_grid for debugging
+        echo mem_grid["0"] & mem_grid["1"] & mem_grid["2"] & mem_grid["3"] & mem_grid["4"] & mem_grid["5"] & mem_grid["6"] & mem_grid["7"] & mem_grid["8"] & mem_grid["9"]
