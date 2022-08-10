0b759b5a0cb4b167ee7357c4a89b3a691c85e83d
Mr.Ender <72358826+MrEnder0@users.noreply.github.com>
2022-08-09 19:19:22 -0500

    Setall command and print command
    +other small things

--- interpreter.nim
+++ interpreter.nim
@@ -1,3 +1,4 @@
+from glyths import nil
 import std/strformat
 import strutils
 import tables
@@ -28,6 +29,17 @@ proc interpret*(parsed_code: string) : string {.discardable.} =
         elif parsed_code[c_index] == 's':
             if len(parsed_code) > c_index + 2:
                 mem_grid[fmt"{parsed_code[c_index + 1]}"] = fmt"{parsed_code[c_index + 2]}"
+
+        #set all command
+        elif parsed_code[c_index] == 'a':
+            if len(parsed_code) > c_index + 1:
+                for i in 0...8:
+                    mem_grid[fmt"{i}"] = fmt"{parsed_code[c_index + 1]}"
+        
+        #print command
+        elif parsed_code[c_index] == 'p':
+            echo "jofdbxn;fdbnbfd;ln"
+            echo glyths.get_glyth(mem_grid["0"] & mem_grid["1"] & mem_grid["2"] & mem_grid["3"] & mem_grid["4"] & mem_grid["5"] & mem_grid["6"] & mem_grid["7"] & mem_grid["8"])
 
         c_index += 1
         #print mem_grid for debugging
