fcd0a92d87600d32b8235ccd6745c572a878a1d9
Mr.Ender <72358826+MrEnder0@users.noreply.github.com>
2022-08-09 21:09:39 -0500

    kind of while command + code changes

--- interpreter.nim
+++ interpreter.nim
@@ -11,6 +11,7 @@ iterator `...`*[T](a: T, b: T): T =
 
 proc interpret*(parsed_code: string) : string {.discardable.} =
     var mem_grid: Table[string, string]
+    var while_pos: int = -1
     var c_index: int = 0
 
     #generate memory grid
@@ -38,10 +39,33 @@ proc interpret*(parsed_code: string) : string {.discardable.} =
         
         #print command
         elif parsed_code[c_index] == 'p':
-            echo "jofdbxn;fdbnbfd;ln"
             echo glyths.get_glyth(mem_grid["0"] & mem_grid["1"] & mem_grid["2"] & mem_grid["3"] & mem_grid["4"] & mem_grid["5"] & mem_grid["6"] & mem_grid["7"] & mem_grid["8"])
 
+        #terminate command
+        elif parsed_code[c_index] == 't':
+            break
+
+        #while command
+        elif parsed_code[c_index] == 'w':
+            if len(parsed_code) > c_index + 3:
+                if parsed_code[c_index + 3] == '0':
+                    if parseint(mem_grid[fmt"{parsed_code[c_index + 1]}"]) == 0:
+                        while_pos = c_index + 1
+                    else:
+                        return
+                if parsed_code[c_index + 3] == '1':
+                    if parseint(mem_grid[fmt"{parsed_code[c_index + 1]}"]) == 1:
+                        while_pos = c_index + 1
+                    else:
+                        return
+    
+        #Go to start of while loop
+        if c_index == len(parsed_code)-1:
+            if while_pos == -1:
+                return
+            else:
+                c_index = while_pos
+        
         c_index += 1
-        #print mem_grid for debugging
-        echo "Frame mem: " & mem_grid["0"] & mem_grid["1"] & mem_grid["2"] & mem_grid["3"] & mem_grid["4"] & mem_grid["5"] & mem_grid["6"] & mem_grid["7"] & mem_grid["8"]
+
     echo "Final mem: " & mem_grid["0"] & mem_grid["1"] & mem_grid["2"] & mem_grid["3"] & mem_grid["4"] & mem_grid["5"] & mem_grid["6"] & mem_grid["7"] & mem_grid["8"]
