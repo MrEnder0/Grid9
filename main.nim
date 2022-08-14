86a0cfcea397aead04eb936fa6b4eee56d09c530
Mr.Ender <72358826+MrEnder0@users.noreply.github.com>
2022-08-14 00:42:35 -0500

    Update main.nim

--- main.nim
+++ main.nim
@@ -18,7 +18,7 @@ proc main() =
     let args = docopt(doc, version = "1.0")
 
     if args["version"] or args["v"]:
-        echo "0.4.0"
+        echo "0.4.2"
 
     if args["interpret"] or args["i"]:
         var parsed_code = code_parser.parse($args["<path>"])
