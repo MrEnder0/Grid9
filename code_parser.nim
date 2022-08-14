3053ac1cc3eaa46875bed808debeb0312dcc8ce9
Mr.Ender <72358826+MrEnder0@users.noreply.github.com>
2022-08-14 00:52:23 -0500

    Ignores upercase characters as comments

--- code_parser.nim
+++ code_parser.nim
@@ -7,7 +7,35 @@ proc parse*(path: string) : string =
 
     while code.read_line(line):
         parsed_code = parsed_code & line
+        
+        #Strips whitespace and all capital letters for comments
         parsed_code = parsed_code.replace(" ", "")
         parsed_code = parsed_code.replace("\n", "")
+        parsed_code = parsed_code.replace("A", "")
+        parsed_code = parsed_code.replace("B", "")
+        parsed_code = parsed_code.replace("C", "")
+        parsed_code = parsed_code.replace("D", "")
+        parsed_code = parsed_code.replace("E", "")
+        parsed_code = parsed_code.replace("F", "")
+        parsed_code = parsed_code.replace("G", "")
+        parsed_code = parsed_code.replace("H", "")
+        parsed_code = parsed_code.replace("I", "")
+        parsed_code = parsed_code.replace("J", "")
+        parsed_code = parsed_code.replace("K", "")
+        parsed_code = parsed_code.replace("L", "")
+        parsed_code = parsed_code.replace("M", "")
+        parsed_code = parsed_code.replace("N", "")
+        parsed_code = parsed_code.replace("O", "")
+        parsed_code = parsed_code.replace("P", "")
+        parsed_code = parsed_code.replace("Q", "")
+        parsed_code = parsed_code.replace("R", "")
+        parsed_code = parsed_code.replace("S", "")
+        parsed_code = parsed_code.replace("T", "")
+        parsed_code = parsed_code.replace("U", "")
+        parsed_code = parsed_code.replace("V", "")
+        parsed_code = parsed_code.replace("W", "")
+        parsed_code = parsed_code.replace("X", "")
+        parsed_code = parsed_code.replace("Y", "")
+        parsed_code = parsed_code.replace("Z", "")
 
     return parsed_code
