c794cbe90e34a3b5af2d76392b3de6591d5122b2
Mr.Ender <72358826+MrEnder0@users.noreply.github.com>
2022-08-14 00:53:29 -0500

    About command line responce
    +better formatting & ver boost

--- main.nim
+++ main.nim
@@ -7,6 +7,8 @@ const doc = """
 CoolSeq - a program to do things
 
 Usage:
+    args_docopt about
+    args_docopt a
     args_docopt version
     args_docopt v
     args_docopt interpret <path>
@@ -17,13 +19,17 @@ Usage:
 proc main() =
     let args = docopt(doc, version = "1.0")
 
+    if args["about"] or args["a"]:
+        echo "\nGrid9 is a esoteric programming language that is based on a 3x3 grid of memory cells where you make patterns glyths.\nThis language created by MrEnder in the Nim programming language.\n"
+
     if args["version"] or args["v"]:
-        echo "0.4.2"
+        echo "\n0.4.3\n"
 
     if args["interpret"] or args["i"]:
+        echo "\n"
         var parsed_code = code_parser.parse($args["<path>"])
         code_interpreter.interpret(parsed_code)
-        echo "Code finished successfully!"
+        echo "\nCode finished successfully!\n"
 
     if args["glyth_value_get"]:
         echo glyths.get_glyth($args["<glyth>"])
