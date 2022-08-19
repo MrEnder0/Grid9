949d5637addcb73b22068f8bfe582635185e4053
Mr.Ender <72358826+MrEnder0@users.noreply.github.com>
2022-08-19 10:07:21 -0500

    huge optimizations made overall

--- code_interpreter.nim
+++ code_interpreter.nim
@@ -1,5 +1,4 @@
 from glyths import nil
-import std/strformat
 import strutils
 import tables
 
@@ -22,29 +21,29 @@ proc interpret*(parsed_code: string) : string {.discardable.} =
 
     #generates a 3x3 memory grid
     for i in 0...8:
-        mem_grid[fmt"{i}"] = "0"
+        mem_grid[$i] = "0"
 
     while c_index < len(parsed_code):
-        #flip command
-        if parsed_code[c_index] == 'f':
+        case $parsed_code[c_index]
+        of $'f':
             if len(parsed_code) > c_index + 1:
-                if parseint(mem_grid[fmt"{parsed_code[c_index + 1]}"]) == 0:
-                    mem_grid[fmt"{parsed_code[c_index + 1]}"] = "1"
+                if parseint(mem_grid[$parsed_code[c_index + 1]]) == 0:
+                    mem_grid[$parsed_code[c_index + 1]] = "1"
                 else:
-                    mem_grid[fmt"{parsed_code[c_index + 1]}"] = "0"
+                    mem_grid[$parsed_code[c_index + 1]] = "0"
         #set command
-        elif parsed_code[c_index] == 's':
+        of $'s':
             if len(parsed_code) > c_index + 2:
-                mem_grid[fmt"{parsed_code[c_index + 1]}"] = fmt"{parsed_code[c_index + 2]}"
+                mem_grid[$parsed_code[c_index + 1]] = $parsed_code[c_index + 2]
 
         #set all command
-        elif parsed_code[c_index] == 'a':
+        of $'a':
             if len(parsed_code) > c_index + 1:
                 for i in 0...8:
-                    mem_grid[fmt"{i}"] = fmt"{parsed_code[c_index + 1]}"
+                    mem_grid[$i] = $parsed_code[c_index + 1]
 
         #queue command
-        elif parsed_code[c_index] == 'q':
+        of $'q':
             if len(parsed_code) > c_index + 1:
                 if parsed_code[c_index + 1] == 's':
                     mem_queue = mem_queue & glyths.get_glyth(mem_grid["0"]&mem_grid["1"]&mem_grid["2"]&mem_grid["3"]&mem_grid["4"]&mem_grid["5"]&mem_grid["6"]&mem_grid["7"]&mem_grid["8"])
@@ -52,7 +51,7 @@ proc interpret*(parsed_code: string) : string {.discardable.} =
                     mem_queue = ""
 
         #print command
-        elif parsed_code[c_index] == 'p':
+        of $'p':
             if mem_queue != "":
                 echo mem_queue
                 mem_queue = ""
@@ -60,12 +59,12 @@ proc interpret*(parsed_code: string) : string {.discardable.} =
                 echo glyths.get_glyth(mem_grid["0"]&mem_grid["1"]&mem_grid["2"]&mem_grid["3"]&mem_grid["4"]&mem_grid["5"]&mem_grid["6"]&mem_grid["7"]&mem_grid["8"])
 
         #if command
-        elif parsed_code[c_index] == 'i':
+        of $'i':
             if len(parsed_code) > c_index + 3:
                 if parsed_code[c_index + 2] == '=':
 
                     if parsed_code[c_index + 3] == '0':
-                        if parseint(mem_grid[fmt"{parsed_code[c_index + 1]}"]) == 0:
+                        if parseint(mem_grid[$parsed_code[c_index + 1]]) == 0:
                             discard
                         else:
                             #skip to end of if
@@ -82,7 +81,7 @@ proc interpret*(parsed_code: string) : string {.discardable.} =
                             c_index = if_pos_end
 
                     if parsed_code[c_index + 3] == '1':
-                        if parseint(mem_grid[fmt"{parsed_code[c_index + 1]}"]) == 1:
+                        if parseint(mem_grid[$parsed_code[c_index + 1]]) == 1:
                             discard
                         else:
                             #skip to end of if
@@ -99,7 +98,7 @@ proc interpret*(parsed_code: string) : string {.discardable.} =
                             c_index = if_pos_end
 
                 elif parsed_code[c_index + 2] == '!':
-                    if parseint(mem_grid[fmt"{parsed_code[c_index + 1]}"]) != parseint(mem_grid[fmt"{parsed_code[c_index + 3]}"]):
+                    if parseint(mem_grid[$parsed_code[c_index + 1]]) != parseint(mem_grid[$parsed_code[c_index + 3]]):
                         discard
                     else:
                         #skip to end of if
@@ -116,39 +115,40 @@ proc interpret*(parsed_code: string) : string {.discardable.} =
                         c_index = if_pos_end
 
         #while command
-        elif parsed_code[c_index] == 'w':
+        of $'w':
             if len(parsed_code) > c_index + 3:
                 if parsed_code[c_index + 2] == '=':
                     if parsed_code[c_index + 3] == '0':
-                        if parseint(mem_grid[fmt"{parsed_code[c_index + 1]}"]) == 0:
+                        if parseint(mem_grid[$parsed_code[c_index + 1]]) == 0:
                             while_pos = c_index + 1
                         else:
                             return
                     if parsed_code[c_index + 3] == '1':
-                        if parseint(mem_grid[fmt"{parsed_code[c_index + 1]}"]) == 1:
+                        if parseint(mem_grid[$parsed_code[c_index + 1]]) == 1:
                             while_pos = c_index + 1
                         else:
                             return
                 elif parsed_code[c_index + 2] == '!':
-                    if parseint(mem_grid[fmt"{parsed_code[c_index + 1]}"]) != parseint(mem_grid[fmt"{parsed_code[c_index + 3]}"]):
+                    if parseint(mem_grid[$parsed_code[c_index + 1]]) != parseint(mem_grid[$parsed_code[c_index + 3]]):
                         while_pos = c_index + 1
                     else:
                         return
 
         #exit command
-        elif parsed_code[c_index] == 'x':
+        of $'x':
             while_pos_end = c_index + 1
             while parsed_code[while_pos_end] != ']':
                 while_pos_end += 1
             c_index = if_pos_end
 
         #back command
-        elif parsed_code[c_index] == 'b':
+        of $'b':
+            {.linearScanEnd.}
             if len(parsed_code) > c_index + 1:
-                c_index -= parseint(fmt"{parsed_code[c_index + 1]}")
+                c_index -= parseint($parsed_code[c_index + 1])
 
         #terminate command
-        elif parsed_code[c_index] == 't':
+        of $'t':
             break
     
         #Go to start of while loop
