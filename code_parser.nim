7ef294670b186e5c62dbbc7257236b46788e0845
MrEnder0 <endermobtrey@gmail.com>
2022-09-10 14:17:57 -0500

    uses regular expression now when parsing

--- code_parser.nim
+++ code_parser.nim
@@ -1,6 +1,7 @@
 import std/strutils
 import std/hashes
 import os
+import re
 
 proc parse*(path: string) : string =
     let code = open(path)
@@ -23,38 +24,7 @@ proc parse*(path: string) : string =
             parsed_code = parsed_code & line
         
             #Strips whitespace and all capital letters for comments
-            parsed_code = parsed_code.replace(" ", "")
-            parsed_code = parsed_code.replace("\n", "")
-            parsed_code = parsed_code.replace("A", "")
-            parsed_code = parsed_code.replace("B", "")
-            parsed_code = parsed_code.replace("C", "")
-            parsed_code = parsed_code.replace("D", "")
-            parsed_code = parsed_code.replace("E", "")
-            parsed_code = parsed_code.replace("F", "")
-            parsed_code = parsed_code.replace("G", "")
-            parsed_code = parsed_code.replace("H", "")
-            parsed_code = parsed_code.replace("I", "")
-            parsed_code = parsed_code.replace("J", "")
-            parsed_code = parsed_code.replace("K", "")
-            parsed_code = parsed_code.replace("L", "")
-            parsed_code = parsed_code.replace("M", "")
-            parsed_code = parsed_code.replace("N", "")
-            parsed_code = parsed_code.replace("O", "")
-            parsed_code = parsed_code.replace("P", "")
-            parsed_code = parsed_code.replace("Q", "")
-            parsed_code = parsed_code.replace("R", "")
-            parsed_code = parsed_code.replace("S", "")
-            parsed_code = parsed_code.replace("T", "")
-            parsed_code = parsed_code.replace("U", "")
-            parsed_code = parsed_code.replace("V", "")
-            parsed_code = parsed_code.replace("W", "")
-            parsed_code = parsed_code.replace("X", "")
-            parsed_code = parsed_code.replace("Y", "")
-            parsed_code = parsed_code.replace("Z", "")
-            parsed_code = parsed_code.replace("(", "")
-            parsed_code = parsed_code.replace(")", "")
-            parsed_code = parsed_code.replace("*", "")
-            parsed_code = parsed_code.replace("-", "")
+            parsed_code = replace(parsed_code, re("[A-Z!?*()\n ]+"), "")
 
         let parsed_code_file = open(parser_cache_dir & $file_hash & ".g9", fmWrite)
         let unparsed_code_file = open(parser_cache_dir & $file_hash & "_pre.g9", fmWrite)
