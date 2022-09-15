5b002f20a3eb1ab44f0199ae49a0d4cfdda35054
MrEnder0 <endermobtrey@gmail.com>
2022-09-14 19:53:03 -0500

    replaced help with example

--- main.nim
+++ main.nim
@@ -8,7 +8,7 @@ const doc = """
 Usage:
     Grid9 (about | a)
     Grid9 (version | v)
-    Grid9 (help | h) <command>
+    Grid9 (example | e) <number>
     Grid9 (clean | c) <folder>
     Grid9 (interpret | i) <path>
     Grid9 glyth_value_get <glyth>
@@ -20,28 +20,8 @@ proc about() =
 proc version() =
     echo "\n2022-012\n"
 
-proc help(command: string) =
-    case $command
-    of $'f':
-        echo "\nThe 'f' command is used to flip a value  on the memory grid and is used as 'f0' the 0 can be any number from 0-8 for the cell id on the grid to be fliped.\n"
-    of $'s':
-        echo "\nThe 's' command is used to set a value on the memory grid and is used as 's00' the first 0 can be any number from 0-8 for the cell id on the grid to be set and the second 0 is the 0 or 1 the cell gets set to.\n"
-    of $'a':
-        echo "\nThe 'a' command is used to set all the values on the memory grid and is used as 'a0' the 0 can be a 0 or a 1 and all the memory grid cells will be set to it.\n"
-    of $'q':
-        echo "\nThe 'q' command is used to store the current glyth (memory grid) as a character added to the end of the queue. The queue will be printed out and cleared when the 'p' command is used. If the letter 'c' is following a queue command it will only clear the queue with no output.\n"
-    of $'p':
-        echo "\nThe 'p' command is used to print the queue if it contains any data or the current glyth if the queue is empty.\n"
-    of $'i':
-        echo "\nThe 'i' command is used as a if statement like 'i0=0' the first 0 is the memory cell id (0-8) the equal sign can also be a ! if you want to use a not equal and the second 0 is the value you are checking the memory grid cell for. Also the if statement is closed by a ].\n"
-    of $'w':
-        echo "\nThe 'w' command is used as a while statement like 'w0=0' it uses the same logic as a if statement so do 'help i' for info on that but this time it will read code until a } and loop back the begining of the while statement if it is still true.\n"
-    of $'x':
-        echo "\nThe 'x' command is used as a break or exit statment where it will skip code until it finds a ] (a end of a while ).\n"
-    of $'b':
-        echo "\nThe 'b' command is used like this 'b0' the 0 can be a 0-9 and will go back that many characters (automatically skips whitespace and upercase characters) and is a basic implimentation of a goto statement.\n"
-    of $'t':
-        echo "\nThe 't' command is used to terminate the current script and not interpret any code past this point unless it gets ignored by a non true if statment or by a break command in a while if its before it.\n"
+proc example(number: string) =
+    case $number
     of "example1":
         echo "\n**This example shows how to use basic language features such as the memory grid, queue and  printing.**\n"
         try:
@@ -61,7 +41,7 @@ proc help(command: string) =
         except:
             echo "Error: example3.g9 not found, maybe you did not install the optional component."
     else:
-        echo "\nNo help found your input try any of the following 'f', 's', 'a', 'q', 'p', 'i', 'w', 'x', 'b', 't' or 'example1', 'example2', 'example3'.\n"
+        echo "\nNo example found for your input try any of the flollowing, 'example1', 'example2', 'example3'.\n"
         
 proc clean(folder: string) =
     when defined windows:
@@ -104,7 +84,7 @@ proc main() =
         version()
     
     if args["help"] or args["h"]:
-        help($args["<command>"])
+        example($args["<number>"])
 
     if args["clean"] or args["c"]:
         clean($args["<folder>"])
