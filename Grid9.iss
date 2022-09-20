8c7723ae29193c890fd3ca9a780a7885225ac7f3
MrEnder0 <endermobtrey@gmail.com>
2022-09-19 21:28:36 -0500

    ver boost

--- Grid9.iss
+++ Grid9.iss
@@ -1,5 +1,5 @@
 #define MyAppName "Grid9"
-#define MyAppVersion "2022-012"
+#define MyAppVersion "2022-013"
 #define MyAppPublisher "MrEnder"
 #define MyAppURL "https://github.com/MrEnder0/Grid9"
 #define MyAppExeName "Grid9.exe"
--- main.nim
+++ main.nim
@@ -22,7 +22,7 @@ proc about() =
     echo "\nGrid9 is a esoteric programming language that is based on a 3x3 grid of memory cells where you make patterns glyths.\nThis language created by MrEnder in the Nim programming language.\n"
 
 proc version() =
-    echo "\n2022-012\n"
+    echo "\n2022-013\n"
 
 proc example(number: string) =
     case $number
@@ -91,7 +91,7 @@ proc glyth_value_get(glyth: string) =
     echo glyths.get_glyth(glyth)
 
 proc main() =
-    let args = docopt(doc, version = "2022-012")
+    let args = docopt(doc, version = "2022-013")
 
     if args["about"] or args["a"]:
         about()
