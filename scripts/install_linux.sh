5340dccd84f33aeb4c1fadd2d5b93f33eef0b8d5
MrEnder0 <endermobtrey@gmail.com>
2022-09-25 22:35:23 -0500

    kali & arch dirrect support

--- scripts/install_linux.sh
+++ scripts/install_linux.sh
@@ -1,16 +1,19 @@
 LINUX_VERSION_NAME=`lsb_release -a`
 
-if [[ $LINUX_VERSION_NAME == *"Ubuntu"* ]];
-then
-    echo "System identified as Ubuntu. Enter sudo password to continue."
+if [[ $LINUX_VERSION_NAME == *"Ubuntu"* ]]; then
+    echo "System identified as Ubuntu. You may have to enter sudo password to continue."
     PACKAGE_MANAGER="apt"
-elif [[ $LINUX_VERSION_NAME == *"Debian"* ]];
-then
-    echo "System identified as Debian. Enter sudo password to continue."
+elif [[ $LINUX_VERSION_NAME == *"Debian"* ]]; then
+    echo "System identified as Debian. You may have to enter sudo password to continue."
     PACKAGE_MANAGER="apt"
-elif [[ $LINUX_VERSION_NAME == *"Manjaro"* ]];
-then
-    echo "System identified as Manjaro. Enter sudo password to continue."
+elif [[ $LINUX_VERSION_NAME == *"Kali"* ]]; then
+    echo "System identified as Kali. You may have to enter sudo password to continue."
+    PACKAGE_MANAGER="apt"
+elif [[ $LINUX_VERSION_NAME == *"Arch"* ]]; then
+    echo "System identified as Arch. You may have to enter sudo password to continue."
+    PACKAGE_MANAGER="pacman"
+elif [[ $LINUX_VERSION_NAME == *"Manjaro"* ]]; then
+    echo "System identified as Manjaro. You may have to enter sudo password to continue."
     PACKAGE_MANAGER="pacman"
 else
     echo "We cant identify your linux version, we currently support apt and pacman package managers if your system does one of these just enter apt or pacman."
@@ -19,7 +22,8 @@ fi
 
 if [[ $PACKAGE_MANAGER == "apt" ]]; then
     sudo apt update
-    sudo apt install git, nim
+    sudo apt install git
+    sudo apt install nim
     nimble install docopt -y
     git clone https://github.com/MrEnder0/Grid9.git
     cd Grid9
@@ -27,12 +31,13 @@ if [[ $PACKAGE_MANAGER == "apt" ]]; then
     export PATH=$PATH:~$PWD/main
 elif [[ $PACKAGE_MANAGER == "pacman" ]]; then
     sudo pacman -Syyu
-    sudo pacman -S git nim
+    sudo pacman -S git
+    sudo pacman -S nim
     nimble install docopt -y
     git clone https://github.com/MrEnder0/Grid9.git
     cd Grid9
     nim c -d:release main
     export PATH=$PATH:~$PWD/main
 else
-    echo "Unsuported package manager only apt and pacman are suported."
+    echo "Unsuported package manager only apt and pacman are curently suported."
 fi
