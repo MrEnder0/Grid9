52892e08f30e604be14a337863caee9555be22b7
Mr.Ender <endermobtrey@gmail.com>
2022-09-29 15:03:23 +0000

    setup changes

--- src/main.nim
+++ src/main.nim
@@ -25,52 +25,55 @@ proc version() =
     echo "\n2022-014\n"
 
 proc example(name: string) =
-    case $name
-    of "example1":
-        echo "\n**This example shows how to use basic language features such as the memory grid, queue and  printing.**\n"
-        try:
-            echo readFile("examples/example1.g9") & "\n"
-        except:
-            echo "Error: example1.g9 not found, maybe you did not install the optional component."
-    of "example2":
-        echo "\n**This example shows how to use if statements and while statements.**\n"
-        try:
-            echo readFile("examples/example2.g9") & "\n"
-        except:
-            echo "Error: example2.g9 not found, maybe you did not install the optional component."
-    of "example3":
-        echo "\n**This example shows how to use the break and goto commands.**\n"
-        try:
-            echo readFile("examples/example3.g9") & "\n"
-        except:
-            echo "Error: example3.g9 not found, maybe you did not install the optional component."
-    of "give_example":
-        echo "\n**This example shows how to use the give command.**\n"
-        try:
-            echo readFile("examples/give_example.g9") & "\n"
-        except:
-            echo "Error: give_example.g9 not found, maybe you did not install the optional component."
-    of "random_char_example":
-        echo "\n**This example shows how to use the random argument for cell modification.**\n"
-        try:
-            echo readFile("examples/random_char_example.g9") & "\n"
-        except:
-            echo "Error: random_char_example.g9 not found, maybe you did not install the optional component."
-    of "while_nesting":
-        echo "\n**This example shows how to use while loops inside of while loops.**\n"
-        try:
-            echo readFile("examples/while_nesting.g9") & "\n"
-        except:
-            echo "Error: while_nesting.g9 not found, maybe you did not install the optional component."
-    of "if_ladder":
-        echo "\n**This example shows how to use a chain of ladders to climb a if chain.**\n"
-        try:
-            echo readFile("examples/if_ladder.g9") & "\n"
-        except:
-            echo "Error: if_ladder.g9 not found, maybe you did not install the optional component."
+    when defined windows:
+        case $name
+        of "example1":
+            echo "\n**This example shows how to use basic language features such as the memory grid, queue and  printing.**\n"
+            try:
+                echo readFile("examples/example1.g9") & "\n"
+            except:
+                echo "Error: example1.g9 not found, maybe you did not install the optional component."
+        of "example2":
+            echo "\n**This example shows how to use if statements and while statements.**\n"
+            try:
+                echo readFile("examples/example2.g9") & "\n"
+            except:
+                echo "Error: example2.g9 not found, maybe you did not install the optional component."
+        of "example3":
+            echo "\n**This example shows how to use the break and goto commands.**\n"
+            try:
+                echo readFile("examples/example3.g9") & "\n"
+            except:
+                echo "Error: example3.g9 not found, maybe you did not install the optional component."
+        of "give_example":
+            echo "\n**This example shows how to use the give command.**\n"
+            try:
+                echo readFile("examples/give_example.g9") & "\n"
+            except:
+                echo "Error: give_example.g9 not found, maybe you did not install the optional component."
+        of "random_char_example":
+            echo "\n**This example shows how to use the random argument for cell modification.**\n"
+            try:
+                echo readFile("examples/random_char_example.g9") & "\n"
+            except:
+                echo "Error: random_char_example.g9 not found, maybe you did not install the optional component."
+        of "while_nesting":
+            echo "\n**This example shows how to use while loops inside of while loops.**\n"
+            try:
+                echo readFile("examples/while_nesting.g9") & "\n"
+            except:
+                echo "Error: while_nesting.g9 not found, maybe you did not install the optional component."
+        of "if_ladder":
+            echo "\n**This example shows how to use a chain of ladders to climb a if chain.**\n"
+            try:
+                echo readFile("examples/if_ladder.g9") & "\n"
+            except:
+                echo "Error: if_ladder.g9 not found, maybe you did not install the optional component."
+        else:
+            echo "\nNo example found for your input try any of the flollowing, 'example1', 'example2', 'example3', 'give_example', 'random_char_example', 'while_nesting', 'if_ladder'.\n"
     else:
-        echo "\nNo example found for your input try any of the flollowing, 'example1', 'example2', 'example3', 'give_example', 'random_char_example', 'while_nesting', 'if_ladder'.\n"
-        
+        echo "\nExample files are currently only implimented on Windows will add Linux support soon."
+
 proc clean(folder: string) =
     when defined windows:
         let main_dir = r"C:\ProgramData\Grid9\"
