0279768f2f657fcac7cd7be14ca7c05055b8b143
MrEnder0 <endermobtrey@gmail.com>
2022-09-30 23:23:47 -0500

    status messages

--- scripts/install_windows.ps1
+++ scripts/install_windows.ps1
@@ -1,9 +1,15 @@
 $url = "https://github.com/dom96/choosenim/releases/download/v0.8.4/choosenim-0.8.4_windows_amd64.exe"
 
+Write-Output "Installing Nim... (0/3)"
 Invoke-WebRequest -Uri $url -OutFile "choosenim-0.8.4_windows_amd64.exe"
 ./choosenim-0.8.4_windows_amd64 --firstInstall stable
 
+Write-Output "Installing dependencies... (1/3)"
 nimble install docopt -y
+
+Write-Output "Compiling Grid9... (2/3)"
 git clone https://github.com/MrEnder0/Grid9.git
 Set-Location Grid9/src
 nim c -d:release main.nim
+
+Write-Output "Finished successfully! (3/3)"
