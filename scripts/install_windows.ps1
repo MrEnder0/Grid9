3376b4f75f49ee4de986257e92a75145d5e25dbc
Mr.Ender <endermobtrey@gmail.com>
2022-10-20 15:50:00 +0000

    overhaul window self build script

--- scripts/install_windows.ps1
+++ scripts/install_windows.ps1
@@ -1,16 +1,28 @@
 $url = "https://github.com/dom96/choosenim/releases/download/v0.8.4/choosenim-0.8.4_windows_amd64.exe"
 
-Write-Output "Installing Nim... (0/3)"
+Write-Output "Installing Nim... (0/5)"
 Invoke-WebRequest -Uri $url -OutFile "choosenim.exe"
 .\choosenim stable --firstInstall
 
-Write-Output "Installing dependencies... (1/3)"
+Write-Output "Installing dependencies... (1/5)"
 nimble install docopt -y
 nimble install yaml -y
 
-Write-Output "Compiling Grid9... (2/3)"
+Write-Output "Compiling Grid9... (2/5)"
 git clone https://github.com/MrEnder0/Grid9.git
 Set-Location Grid9/src
 nim c -d:release main.nim
 
-Write-Output "Finished! (3/3)"
+Write-Output "Installing Grid9... (3/5)"
+Rename-Item -Path "main.exe" -NewName "Grid9.exe"
+New-Item "C:\Program Files (x86)\Grid9" -itemType Directory
+Move-Item -Path "Grid9.exe" -Destination "C:\Program Files (x86)\Grid9"
+New-Item "C:\ProgramData\Grid9" -itemType Directory
+
+Write-Output "Installing Examples... (4/5)"
+Move-Item -Path "examples" -Destination "C:\ProgramData\Grid9"
+
+Write-Output "Installing documentation... (5/5)"
+Move-Item -Path "documentation" -Destination "C:\ProgramData\Grid9"
+
+Write-Output "Finished!"
