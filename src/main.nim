2e42711b69685707ff85da8dff67c44f2ddfbac9
MrEnder0 <endermobtrey@gmail.com>
2022-11-01 23:09:44 -0500

    added todo comment

--- src/main.nim
+++ src/main.nim
@@ -179,6 +179,7 @@ proc interpret*(path: string, advancedParse: bool, dontCache: bool, echoGridMod:
         echo "Version: " & version & "\n"
 
     #Parse and interpret the code
+    #TODO: Make the verbosity system work
     let parsedCode = code_parser.parse(path, advancedParseY, dontCacheY , noLogY)
     code_interpreter.interpret(parsedCode, echoGridModY, noLogY)
     echo "\nCode finished successfully!"
